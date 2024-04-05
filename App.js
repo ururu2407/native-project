648c6f4949304b034e9a5889e921a6de8a8f6593
Oleksiy <122101396+ururu2407@users.noreply.github.com>
2024-04-05 20:27:07 +0300

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { HomeScreen } from './screens/Home';
-import { StatusBar, View } from 'react-native';
-import { FullPostScreen } from './screens/FullPost';
-import { Navigation } from './screens/Navigation';
-
-export default function App() {
-    return <Navigation/>
-}
